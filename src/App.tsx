4bfe382627d801ecf388e26f2c521fc3f7befd4b
Oktay <oktay@yatko.dev>
2022-03-07 22:03:10 +0100

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import type { Component } from "solid-js";
 
 const App: Component = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="text-6xl text-indigo-400 font-bold grid place-items-center min-h-screen bg-zinc-900">
+      Hello World
+    </h1>
+  );
 };
 
 export default App;
