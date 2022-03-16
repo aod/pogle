03cb87a71d9fb2e1713ea7049010a0e46848771c
Oktay <oktay@yatko.dev>
2022-03-16 16:41:39 +0100

    Replace 'o' in 'Pogle' with the emote in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import type { Component } from "solid-js";
 
+import PagMan from "../assets/pagman.png";
+
 const Navbar: Component = () => {
   return (
     <nav class="border-b border-zinc-600">
+      <h1 class="absolute h-px w-px overflow-hidden text-transparent">Pogle</h1>
       <div className="h-14 max-h-14 container mx-auto flex items-center justify-center">
         <a
-          class="text-zinc-200 text-3xl font-bold tracking-wide uppercase"
+          class="text-zinc-200 text-3xl font-bold tracking-wide uppercase flex items-center gap-1"
           href={window.location.href}
         >
-          <h1>Pogle</h1>
+          <span>P</span>
+          <img src={PagMan} alt="PagMan" class="h-8" />
+          <span>gle</span>
         </a>
       </div>
     </nav>
