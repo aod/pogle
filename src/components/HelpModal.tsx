fd935e7367a30fc063feaea84dbba78a8a43de8b
Oktay <oktay@yatko.dev>
2022-03-17 13:20:33 +0100

    Center title in modals

--- src/components/HelpModal.tsx
+++ src/components/HelpModal.tsx
@@ -6,7 +6,7 @@ import Tile from "./board/Tile";
 const HelpModal: Component = () => {
   return (
     <>
-      <h1 class="uppercase">How to play</h1>
+      <h1 class="uppercase text-center">How to play</h1>
       <p>Guess the daily Twitch emote in six tries.</p>
       <p>
         Each guess must be a valid five-letter Twitch/BTTV/FFZ/7TV emote. Hit
@@ -23,9 +23,11 @@ const HelpModal: Component = () => {
         <Tile spot={Spot.Wrong}>P</Tile>
         <Tile spot={Spot.None}>A</Tile>
       </p>
-      The letter <b>K</b> is in the word and in the correct spot. The letter{" "}
-      <b>P</b> is in the word but in the wrong spot. The letters A is not in the
-      word in any spot.
+      <p>
+        The letter <b>K</b> is in the word and in the correct spot. The letter{" "}
+        <b>P</b> is in the word but in the wrong spot. The letters A are not in
+        the word in any spot.
+      </p>
     </>
   );
 };
