9983cffa0c3dcae9b1b78d30c6355dc6be453c45
Oktay <oktay@yatko.dev>
2022-03-21 13:55:47 +0100

    Decrease padding on main pogle component

--- src/components/Pogle.tsx
+++ src/components/Pogle.tsx
@@ -7,7 +7,7 @@ import { hasLost, todaysRandomEmote } from "../state";
 
 const Pogle: Component = () => {
   return (
-    <main class="container mx-auto px-2 pt-6 sm:pt-8 pb-2 sm:pb-4 flex flex-grow flex-col items-center gap-6 sm:gap-8 h-full justify-between">
+    <main class="container mx-auto px-2 py-2 sm:pb-4 flex flex-grow flex-col items-center h-full justify-between">
       <Show when={hasLost()} fallback={<div />}>
         <span class="text-zinc-800 font-semibold uppercase tracking-wide px-4 sm:px-8 py-2 sm:py-4 bg-zinc-200 rounded-lg sm:text-xl">
           {todaysRandomEmote}
